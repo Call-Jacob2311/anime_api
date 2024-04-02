dbee6370cae60a925593d933203dc316fdec1691
Call-Jacob <dehholio@gmail.com>
2024-04-01 20:10:04 -0600

    Updates to project
    -- Created shared library
    -- Created test project

---
+++ anime_api/TestProject1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace anime_api_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ anime_api/anime_api/Controllers/anime_controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace anime_api.Controllers
+{
+    public class anime_controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
