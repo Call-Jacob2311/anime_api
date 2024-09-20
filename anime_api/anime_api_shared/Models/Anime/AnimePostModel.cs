bbfb40f5473785bfb19b5ce097349930d810bfb4
Call-Jacob <dehholio@gmail.com>
2024-09-19 19:25:07 -0600

    Reapply "Fixed Get and Post endpoint"
    This reverts commit 007b38f09f9292ae29c562eefad8482b0ac78270.

--- anime_api/anime_api_shared/Models/Anime/AnimePostModel.cs
+++ anime_api/anime_api_shared/Models/Anime/AnimePostModel.cs
@@ -16,8 +16,8 @@ namespace anime_api_shared.Models.Anime
         /// Gets or sets the status of the anime (e.g., Ongoing, Completed).
         /// </summary>
         [Required]
-        [StringLength(50, ErrorMessage = "Anime status can't be longer than 50 characters.")]
-        public string AnimeStatus { get; set; } = string.Empty;
+        [Range(1, int.MaxValue, ErrorMessage = "Episode count must be greater than 0.")]
+        public int AnimeStatusId { get; set; }
 
         /// <summary>
         /// Gets or sets the id of the studio that produced the anime.
@@ -44,5 +44,47 @@ namespace anime_api_shared.Models.Anime
         [Required]
         [StringLength(200, ErrorMessage = "Genres can't be longer than 200 characters.")]
         public string Genres { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the thumb nail link for the anime.
+        /// </summary>
+        [Required]
+        [StringLength(200, ErrorMessage = "Thumb nail link can't be longer than 200 characters.")]
+        public string ThumbnailLink { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the the season for the anime.
+        /// </summary>
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Episode count must be greater than 0.")]
+        public int SeasonId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the the ost for the anime.
+        /// </summary>
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Anime OST id count must be greater than 0.")]
+        public int AnimeOSTId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the the ost for the anime.
+        /// </summary>
+        [Required]
+        [StringLength(200, ErrorMessage = "Thumb nail link can't be longer than 200 characters.")]
+        public string TrueName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the the ost for the anime.
+        /// </summary>
+        [Required]
+        [StringLength(200, ErrorMessage = "Thumb nail link can't be longer than 200 characters.")]
+        public string WikiUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the the ost for the anime.
+        /// </summary>
+        [Required]
+        [StringLength(200, ErrorMessage = "Thumb nail link can't be longer than 200 characters.")]
+        public string ShortHandNames { get; set; } = string.Empty;
     }
 }
